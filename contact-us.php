d7a17e2a826f99e9f9c86845c2757d3062a658ff
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2024-07-08 08:39:29 -0400

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from azim.commonsupport.com/Recvite/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 03 Jul 2024 16:25:09 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <title>Get in Touch with Alpacam</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Contact Alpacam for personalized recruitment solutions. Whether you're looking for talent or seeking new opportunities, our team is here to assist you.">
+    <meta name="Keywords" content="alpacam,Leadership,Improving,Recruitement,Welcome to Alpacam,Accounts Recruitment,Marketing Recruitment,Production Recruitment,Engineering Recruitment,IT Recruitment,Software Recruitment,Legal Recruitment,Andheri West.">
+
+    <meta name="robots" content="index, follow">
+    <meta name="revisit-after" content="1 day">
+    <meta name="language" content="English">
+    <meta name="generator" content="N/A">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Pragma" content="no-cache" />
+
+    <!-- Fav Icon -->
+    <link rel="icon" href="assets/images/icons/favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&amp;display=swap" rel="stylesheet">
+
+    <!-- bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!--  font icon link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Stylesheets -->
+    <link href="assets/css/font-awesome-all.css" rel="stylesheet">
+    <link href="assets/css/flaticon.css" rel="stylesheet">
+    <link href="assets/css/owl.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/jquery.fancybox.min.css" rel="stylesheet">
+    <link href="assets/css/animate.css" rel="stylesheet">
+    <link href="assets/css/color.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/responsive.css" rel="stylesheet">
+
+</head>
+
+
+<!-- page wrapper -->
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <!-- Page Title -->
+    <section class="page-title centred element" style="background-image: url(assets/images/resource/bd-2.jpg);">
+        <div class="auto-container">
+            <div class="content-box">
+                <div class="title">
+                    <h1>Contact us</h1>
+                </div>
+                <ul class="bread-crumb clearfix">
+                    <li><a href="https://www.alpacam.com/">Home</a></li>
+                    <li>Contact us</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <!-- End Page Title -->
+
+
+    <!-- contact-section -->
+    <section class="contact-section sec-pad" id="meet">
+        <div class="auto-container">
+            <div class="row clearfix">
+                <div class="col-lg-8 col-md-12 col-sm-12 form-column">
+                    <img src="assets/images/resource/threed3.png">
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 title-column pdt" style="margin-top: -20px; padding-bottom: 10px;">
+                    <div class="title-inner">
+                        <div class="sec-title">
+                            <p>Contact with Us</p>
+                            <h5><span style="font-weight: bold;">Welcome To Alpacam</span></h5>
+                        </div>
+                        <div class="">
+                            <p>Connecting talent with opportunities, Alpacam ensures efficient recruitment across various industries. Whether you're hiring or job seeking, we're here to help you succeed.</p>
+                        </div>
+                        <div class="widget-content pt-1">
+                                  <div class="right">
+                                      <ul class="residence">
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa-solid fa-phone ic"></i></div>
+                                                  <div class="text"> <a href="tel:00919967634742" class="text-secondary"> +91-9967634742</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa-solid fa-phone ic"></i></div>
+                                                  <div class="text"><a href="tel:00919819166885" class="text-secondary"> +91-9819166885</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa-solid fa-phone ic"></i></div>
+                                                  <div class="text"><a href="tel:00918452016976" class="text-secondary"> +91-8452016976</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa fa-envelope ic"></i></div>
+                                                  <div class="text"><a href="mailto:hr@alpacam.com" class="text-secondary"> hr@alpacam.com</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fas fa-map-marker-alt ic"></i></div>
+                                                  <div class="text"><a class="text-secondary"> No.1201, Poonam Tower, Veera Desai Road, Andheri West, Mumbai 400 053, India.</a></div>
+                                              </div>
+                                          </li>
+                                          
+                                      </ul>
+                                  </div>
+                              </div>
+
+                        <!-- <ul class="social-links clearfix">
+                            <li>
+                                <h6>Connect:</h6>
+                            </li>
+                            <li><a href="contact.html"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="contact.html"><i class="fab fa-facebook-square"></i></a></li>
+                            <li><a href="contact.html"><i class="fab fa-pinterest-p"></i></a></li>
+                            <li><a href="contact.html"><i class="fab fa-instagram"></i></a></li>
+                        </ul> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- contact-section end -->
+
+
+    <!-- google-map-section -->
+    <!-- <section class="google-map-section">
+        <div class="above-sidebar-wrapper">
+            <section id="content-section-1">
+                <div class="greennature-full-size-wrapper gdlr-show-all no-skin" style="padding-top: 18px; padding-bottom: 0px;  background-color: #ffffff; ">
+                    <div class="greennature-item greennature-content-item" style="margin-bottom: 0px;">
+                        <iframe style="width:100%; height:480px; border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.286492960109!2d72.83563357425341!3d19.138932649995738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63b26b65db5%3A0xbb70a7be13357e19!2sSamarkand%2C%20Veera%20Desai%20Rd%2C%20Shastri%20Nagar%2C%20Andheri%20West%2C%20Mumbai%2C%20Maharashtra%20400102%2C%20India!5e0!3m2!1sen!2sbd!4v1720286954471!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <div style="position: absolute;width: 80%;bottom: 20px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;">
+                        </div>
+                        <div class="clear"></div>
+                        <div class="clear"></div>
+                    </div>
+                    <div class="clear"></div>
+            </section>
+        </div>
+    </section> -->
+    <!-- google-map-section end -->
+
+
+
+
+
+
+    <?php include 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,153 @@
+  <!-- main-footer -->
+  <footer class="main-footer pdtf">
+      <div class="footer-top" style="background-image: url(assets/images/background/footer-1.jpg);">
+          <div class="auto-container">
+              <div class="footer-info clearfix">
+                  <div class="single-item">
+                      <div class="inner">
+                          <div class="icon-box"><i class="flaticon-mail"></i></div>
+                          <h6>Email</h6>
+                          <p><a href="mailto:hr@alpacam.com">hr@alpacam.com</a></p>
+                      </div>
+                  </div>
+                  <div class="single-item">
+                      <div class="inner">
+                          <div class="icon-box"><i class="flaticon-phone"></i></div>
+                          <h6>Call</h6>
+                          <p><a href="tel:00919967634742">+91-99676 34742</a></p>
+                      </div>
+                  </div>
+                  <div class="single-item">
+                      <div class="inner">
+                          <div class="icon-box"><i class="flaticon-phone"></i></div>
+                          <h6>Call</h6>
+                          <p><a href="tel:00919819166885">+91-98191 66885</a></p>
+                      </div>
+                  </div>
+              </div>
+              <div class="widget-section">
+                  <div class="row clearfix">
+                      <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+                          <div class="footer-widget logo-widget">
+                              <figure class="footer-logo"><a href="https://www.alpacam.com/"><img src="assets/images/icons/logo.png" alt=""></a></figure>
+                              <p>Welcome to Alpacam Human Resources, your trusted partner in comprehensive recruitment services. We specialize in identifying and placing top talent across various industries, both within India and internationally.</p>
+                          </div>
+                      </div>
+                      <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+                          <div class="footer-widget links-widget t-center" style="margin-top: 20px;">
+                              <div class="widget-title">
+                                  <h4>Explore</h4>
+                              </div>
+                              <div class="widget-content">
+                                  <ul class="links-list clearfix">
+                                      <li><a href="https://www.alpacam.com/">Home</a></li>
+                                      <li><a href="about-us.php">About us</a></li>
+                                      <li><a href="contact-us.php">Contact us</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="col-lg-4 col-md-6 col-sm-12 footer-column">
+                          <div class="footer-widget links-widget" style="margin-top: 20px;">
+                              <div class="widget-title">
+                                  <h4>Address</h4>
+                              </div>
+                              <div class="widget-content">
+                                  <div class="right">
+                                      <ul class="residence">
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa-solid fa-phone"></i></div>
+                                                  <div class="text"><a href="tel:00919967634742" class="text-color"> +91-9967634742</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fa fa-envelope"></i></div>
+                                                  <div class="text"><a href="mailto:hr@alpacam.com" class="text-color"> hr@alpacam.com</a></div>
+                                              </div>
+                                          </li>
+                                          <li>
+                                              <div class="icone-text">
+                                                  <div class="icone"><i class="fas fa-map-marker-alt"></i></div>
+                                                  <div class="text"><a class="text-color"> No.1201, Poonam Tower, Veera Desai Road, Andheri West, Mumbai 400 053, India.</a></div>
+                                              </div>
+                                          </li>
+                                          
+                                      </ul>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+              </div>
+              <div class="row text-center mr-auto">
+                  <div class="col-lg-12 mb-2">
+                      <span class="copyrights-text"> Copyright ©
+                          <script>
+                              document.write(new Date().getFullYear())
+                          </script>
+                          <span style="color: #50eb6a">Alpacam Human Resources</span>, All Rights and Reserved. Powered by <a href="https://www.itarsia.com/" target="_blank" style="color: #50eb6a"> Itarsia India Limited.</a>
+                      </span>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+  </footer>
+  <!-- main-footer end -->
+
+
+
+
+  <!--Scroll to top-->
+  <button class="scroll-top scroll-to-target" data-target="html">
+      <span class="fa fa-arrow-up"></span>
+  </button>
+  </div>
+
+
+  <!-- jequery plugins -->
+  <script src="assets/js/jquery.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/owl.js"></script>
+  <script src="assets/js/wow.js"></script>
+  <script src="assets/js/validation.js"></script>
+  <script src="assets/js/jquery.fancybox.js"></script>
+  <script src="assets/js/appear.js"></script>
+  <script src="assets/js/scrollbar.js"></script>
+
+  <!-- main-js -->
+  <script src="assets/js/script.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+  <script type="text/javascript">
+      $(document).ready(function() {
+          $('.desktop-slider').slick({
+              autoplay: true,
+              autoplaySpeed: 2000,
+              prevArrow: '<span class="prev">&#10094;</span>',
+              nextArrow: '<span class="next">&#10095;</span>'
+          });
+
+          $('.mobile-slider').slick({
+              autoplay: true,
+              autoplaySpeed: 2000,
+              arrows: false,
+              dots: true
+          });
+      });
+  </script>
+
+
+
+
+  </body><!-- End of .page_wrapper -->
+
+  <!-- Mirrored from azim.commonsupport.com/Recvite/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 03 Jul 2024 16:25:42 GMT -->
+
+  </html>
